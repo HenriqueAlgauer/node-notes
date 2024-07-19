1fe614c837cf53ca119223d0a16963a2efff76ea
Henrique Machado Algauer <102268957+HenriqueAlgauer@users.noreply.github.com>
2024-07-19 18:12:18 +0000

    rota user com parametros

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
 const app = express();
 
+app.get("/msg/:id/:user", (request, response)=>{
+    const {id, user} = request.params;
+    
+    response.send(`
+        Id da msg: ${id},
+        Para o usuário: ${user}
+        `);
+});
+
+app.get("/users", (request, response)=>{
+    const {page, limit} = request.query;
+
+    response.send(`Página: ${page}. Mostrar:${limit}`)
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
