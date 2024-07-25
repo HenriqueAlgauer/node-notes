404a9ab030c5061bec6c5df025795d860ada4762
Henrique Machado Algauer <102268957+HenriqueAlgauer@users.noreply.github.com>
2024-07-25 11:51:04 +0000

    criado a tabela notes

--- knexfile.js
+++ knexfile.js
@@ -4,8 +4,11 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve
-    }
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    migrations:{
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
   }
-
 };
