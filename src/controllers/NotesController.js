ad1e7866680f4ed0abe589d7e4e71026dc6f02d6
Henrique <algauer.henrique@gmail.com>
2024-07-26 16:37:03 -0300

    att

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,38 +1,38 @@
-const { response } = require('express');
-const knex = require('../database/knex')
+const { response } = require("express");
+const knex = require("../database/knex");
 
-class NotesController{
-    async  create(request, body){
-        const { title, description, tags, links } = request.body;
-        const {user_id} = request.params;
+class NotesController {
+  async create(request, body) {
+    const { title, description, tags, links } = request.body;
+    const { user_id } = request.params;
 
-        const note_id = await knex("notes").insert({
-            title,
-            description,
-            user_id
-        })
+    const note_id = await knex("notes").insert({
+      title,
+      description,
+      user_id,
+    });
 
-        const linksInsert = links.map(link =>{
-            return {
-                note_id,
-                url: link
-            }
-        })
+    const linksInsert = links.map(link => {
+      return {
+        note_id,
+        url: link,
+      };
+    });
 
-        await knex("links").insert(linksInsert);
+    await knex("links").insert(linksInsert);
 
-        const tagsInsert = tags.map(name =>{
-            return {
-                note_id,
-                name,
-                user_id
-            }
-        })
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id,
+      };
+    });
 
-        await knex("tags").insert(tagsInsert)
+    await knex("tags").insert(tagsInsert);
 
-        response.json()
-    }
+    response.json();
+  }
 }
 
 module.exports = NotesController;
