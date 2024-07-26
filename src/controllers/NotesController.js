ddc4e63a0c47199a6be4804e741f3d86cb6acd2e
Henrique <algauer.henrique@gmail.com>
2024-07-26 16:41:39 -0300

    arrumado bug foreign key

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -2,7 +2,7 @@ const { response } = require("express");
 const knex = require("../database/knex");
 
 class NotesController {
-  async create(request, body) {
+  async create(request, response) {
     const { title, description, tags, links } = request.body;
     const { user_id } = request.params;
 
