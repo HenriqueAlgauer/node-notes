9eae71a304e828b42cbbe12600b5059585562364
Henrique <algauer.henrique@gmail.com>
2024-08-14 17:55:04 -0300

    funcao upt att

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -27,11 +27,13 @@ class UsersController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
-    const { id } = request.params;
+    const { name, email, password, old_password } = request.body;
+    const user_id = request.user.id;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
+      user_id,
+    ]);
 
     if (!user) {
       throw new AppError("Usuário não encontrado");
@@ -45,17 +47,34 @@ class UsersController {
       throw new AppError("Este email já está em uso");
     }
 
-    user.name = name;
-    user.email = email;
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha"
+      );
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere");
+      }
+
+      user.password = await hash(password, 8);
+    }
 
     await database.run(
       `
       UPDATE users SET
       name = ?,
       email = ?,
-      updated_at = ?
+      password = ?,
+      updated_at = DATETIME('now')
       WHERE id = ?`,
-      [user.name, user.email, new Date(), id]
+      [user.name, user.email, user.password, user_id]
     );
     return response.json();
   }
