187588610e3f515c69216c4cff50d8aa82be2d17
Henrique <algauer.henrique@gmail.com>
2024-08-16 08:44:30 -0300

    ataulização users routes

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,12 +1,25 @@
 const { Router } = require("express");
+const multer = require("multer");
+const uploadConfig = require("../configs/updload");
 
 const UsersController = require("../controllers/UsersController");
 const ensureAuth = require("../middleware/ensureAuth");
 
 const usersRoutes = Router();
+const upload = multer(uploadConfig.MULTER);
+
 const usersController = new UsersController();
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuth, usersController.update);
+usersRoutes.patch(
+  "/avatar",
+  ensureAuth,
+  upload.single("avatar"),
+  (request, response) => {
+    console.log(request.file.filename);
+    response.json();
+  }
+);
 
 module.exports = usersRoutes;
