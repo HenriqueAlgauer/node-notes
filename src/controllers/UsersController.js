9b73156b47b97d236de81c91ff780edcdddd5eb1
henriqueAlgauer <algauer.henrique@gmail.com>
2024-09-08 18:44:16 -0300

    atualizado script vercel-build

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,16 +1,12 @@
 const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-const sqliteConnection = require("../database/sqlite");
+const knex = require("../database/knex");
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    );
+    const checkUserExists = await knex("users").where({ email }).first();
 
     if (checkUserExists) {
       throw new AppError("Este email já está em uso");
@@ -18,10 +14,11 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8);
 
-    await database.run(
-      "INSERT INTO users (name, email, password) VALUES (?,?,?)",
-      [name, email, hashedPassword]
-    );
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword,
+    });
 
     return response.status(201).json();
   }
@@ -30,20 +27,18 @@ class UsersController {
     const { name, email, password, old_password } = request.body;
     const user_id = request.user.id;
 
-    const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", [
-      user_id,
-    ]);
+    const user = await knex("users").where({ id: user_id }).first();
 
     if (!user) {
       throw new AppError("Usuário não encontrado");
     }
 
-    const userWithUpdatedEmail = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    );
-    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+    const userWithUpdatedEmail = await knex("users")
+      .where({ email })
+      .andWhereNot({ id: user_id })
+      .first();
+
+    if (userWithUpdatedEmail) {
       throw new AppError("Este email já está em uso");
     }
 
@@ -66,16 +61,15 @@ class UsersController {
       user.password = await hash(password, 8);
     }
 
-    await database.run(
-      `
-      UPDATE users SET
-      name = ?,
-      email = ?,
-      password = ?,
-      updated_at = DATETIME('now')
-      WHERE id = ?`,
-      [user.name, user.email, user.password, user_id]
-    );
+    await knex("users")
+      .update({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+        updated_at: knex.fn.now(),
+      })
+      .where({ id: user_id });
+
     return response.json();
   }
 }
